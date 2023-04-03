5389ea4f5ef698770a8ef36d7c2668480fe0b733
Taylor Blau <me@ttaylorr.com>
2023-04-03 16:16:32 -0400

    ref-filter.c: extract match_name_as_path_1()

--- ref-filter.c
+++ ref-filter.c
@@ -2092,6 +2092,24 @@ static int match_pattern(const struct ref_filter *filter, const char *refname)
 	return 0;
 }
 
+static int match_name_as_path_1(const char *pattern, const char *refname,
+				unsigned flags)
+{
+	/* XXX: should be size_t's. */
+	int plen = strlen(pattern);
+	int namelen = strlen(refname);
+
+	if ((plen <= namelen) &&
+	    !strncmp(refname, pattern, plen) &&
+	    (refname[plen] == '\0' ||
+	     refname[plen] == '/' ||
+	     pattern[plen-1] == '/'))
+		return 1;
+	if (!wildmatch(pattern, refname, flags))
+		return 1;
+	return 0;
+}
+
 /*
  * Return 1 if the refname matches one of the patterns, otherwise 0.
  * A pattern can be path prefix (e.g. a refname "refs/heads/master"
@@ -2101,23 +2119,13 @@ static int match_pattern(const struct ref_filter *filter, const char *refname)
 static int match_name_as_path(const struct ref_filter *filter, const char *refname)
 {
 	const char **pattern = filter->name_patterns;
-	int namelen = strlen(refname);
 	unsigned flags = WM_PATHNAME;
 
 	if (filter->ignore_case)
 		flags |= WM_CASEFOLD;
 
 	for (; *pattern; pattern++) {
-		const char *p = *pattern;
-		int plen = strlen(p);
-
-		if ((plen <= namelen) &&
-		    !strncmp(refname, p, plen) &&
-		    (refname[plen] == '\0' ||
-		     refname[plen] == '/' ||
-		     p[plen-1] == '/'))
-			return 1;
-		if (!wildmatch(p, refname, flags))
+		if (match_name_as_path_1(*pattern, refname, flags))
 			return 1;
 	}
 	return 0;
